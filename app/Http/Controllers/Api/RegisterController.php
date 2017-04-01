73db6bae8ec70f8a07cfcd7599e41c5fc4f9ea5e
zmecust <247281377@qq.com>
2017-04-01 19:04:55 +0800

    test again

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -15,7 +15,7 @@ class RegisterController extends ApiController
      */
     public function register(Request $request)
     {
-        dd('test');
+        dd('test 2');
         $rules = [
             'name' => 'required|unique:users|between:4,12',
             'email' => 'required|email|unique:users',
