8b28fa8c2df96c1e11d8273fa32978764b54781d
zmecust <247281377@qq.com>
2017-04-17 18:47:20 +0800

    add tree

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,8 @@ use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
 {
-
+    public function menus()
+    {
+        return $this->belongsToMany(Menu::class)->withTimestamps();
+    }
 }
---
+++ database/migrations/2017_04_17_183244_create_menus_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('menu_role', function (Blueprint $table) {
+            $table->integer('menu_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+
+            $table->foreign('menu_id')->references('id')->on('menus')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['menu_id', 'role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+        Schema::drop('menu_role');
+    }
+}
