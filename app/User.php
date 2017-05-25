248e1554c5a83b44b7b4d2e419be6697526c0e71
zmecust <247281377@qq.com>
2017-05-25 23:11:56 +0800

    first commit

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id', 'name', 'email', 'avatar'
+        'id', 'name', 'email', 'avatar', 'password'
     ];
 
     /**
