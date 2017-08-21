c77fe0847647e7d80766e71f213095d5a68ed0b9
zhangmin <zhangmin@inesatech.local>
2017-08-21 11:15:02 +0800

    modify comment api

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -19,58 +19,38 @@ class CommentsController extends Controller
 
     public function index($id)
     {
-        /*$comments = Comment::where('commentable_id', $id)->where('parent_id', $parent_id)->with('user')->get()->toArray();
-        $new_comments = [];
-
-        if (! empty($comments)) {
-            foreach ($comments as $comment) {
-                if ($comment['parent_id'] !== 0) {
-                    $comment['parent_name'] = Comment::where('id', $comment['parent_id'])->first()->user()->first()->name;
-                }
-                $new_comments[$comment['id']] = $comment;
-                $comment_child = $this->index($id, $comment['id']);
-
-                if (! empty($comment_child)) {
-                    $new_comments[$comment['id']]['children'] = array_values($comment_child);
-                }
-            }
-        }
-
-        return $new_comments;*/
         $comments = Comment::where('commentable_id', $id)->where('parent_id', 0)->with(['user' => function ($query) {
-            $query->select('id', 'name', 'avatar', 'created_at');
+            $query->select('id', 'name', 'avatar');
         }])->get();
-        /*$comments = collect($comments)->map(function ($comment) {
-            if ($comment['parent_id'] !== 0) {
-                $parent_comment = Comment::where('id', $comment['parent_id'])->first()->user()->first();
-                return array_merge(
-                    $comment->toArray(), [
-                      'parent_name' => $parent_comment->name,
-                      'parent_user_id' => $parent_comment->id
-                    ]
-                );
-            }
-            return $comment->toArray();
-        });*/
         return $this->responseSuccess('OK', $comments);
     }
 
     public function childComments($id)
     {
-        $comments = Comment::where('commentable_id', $id)->where('parent_id', Request('parent_id'))
+        $parent_id = Request('parent_id');
+        $comments = $this->getChildComments($id, $parent_id);
+        return $this->responseSuccess('查询成功', $comments);
+    }
+
+    protected function getChildComments($id, $parent_id)
+    {
+        $comments = Comment::where('commentable_id', $id)->where('parent_id', $parent_id)
             ->with(['user' => function ($query) {
-            $query->select('id', 'name', 'avatar', 'created_at');
+            $query->select('id', 'name');
         }])->get();
-        $comments = collect($comments)->map(function ($comment) {
-            $parent_comment = Comment::where('id', $comment['parent_id'])->first()->user()->first();
-            return array_merge(
-                $comment->toArray(), [
-                    'parent_name' => $parent_comment->name,
-                    'parent_user_id' => $parent_comment->id
-                ]
-            );
-        });
-        return $this->responseSuccess('OK', $comments);
+        $new_comments = [];
+
+        if (! empty($comments)) {
+            foreach ($comments as $comment) {
+                $comment['parent_name'] = Comment::where('id', $comment['parent_id'])->first()->user()->first()->name;
+                $new_comments[] = $comment;
+                $comment_child = $this->getChildComments($id, $comment['id']);
+                if (! empty($comment_child)) {
+                    $new_comments[] = array_values($comment_child);
+                }
+            }
+        }
+        return $new_comments;
     }
 
     public function store()
