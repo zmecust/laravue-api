0a017341870f7bab677b0d3082bb0cf96cdfcea3
zhangmin <zhangmin@inesatech.local>
2017-11-22 16:54:56 +0800

    modified the permission create

--- app/Permission.php
+++ app/Permission.php
@@ -19,7 +19,7 @@ class Permission extends EntrustPermission
     {
         return [
             'name' => 'required|unique:permissions,name,'.$id,
-            'display_name' => 'required|unique:permissions,display_name,'.$id,
+            'display_name' => 'required',
             'uri' => 'required'
         ];
     }
@@ -33,7 +33,6 @@ class Permission extends EntrustPermission
             'name.required' => '权限名不能为空',
             'name.unique' => '该权限名已存在',
             'display_name.required' => '权限标识不能为空',
-            'display_name.unique' => '该权限标识已存在',
             'uri.required' => '绑定前端路由名不能为空',
         ];
     }
